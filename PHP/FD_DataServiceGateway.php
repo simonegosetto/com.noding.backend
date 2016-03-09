f4ec6dc14b943a099bf14b433f58a05d34b328bd
FlexibleDevs <flexibledevs@gmail.com>
2016-03-09 18:34:27 +0100

    Simone - modifiche al backend php

--- PHP/FD_DataServiceGateway.php
+++ PHP/FD_DataServiceGateway.php
@@ -25,21 +25,20 @@
 
 header('Content-Type: application/json');
 
-// Allow from any origin
+//Imposto qualsiasi orgine da cui arriva la richiesta come abilitata e la metto in cache per un giorno
 if (isset($_SERVER['HTTP_ORIGIN'])) {
     header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
     header('Access-Control-Allow-Credentials: true');
     header('Access-Control-Max-Age: 86400');    // cache for 1 day
 }
-// Access-Control headers are received during OPTIONS requests
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 
+//Imposto tutti i metodi come abilitati
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
         header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 
     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
         header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-
 }
 
 include "FD_Mysql.php";
