e5d2ff050409120a2fd9d5e85ed7650be42885a0
FlexibleDevs <flexibledevs@gmail.com>
2016-04-06 14:40:51 +0200

    Simone - sviluppo dello servizio di validazione degli utenti per GET

--- PHP/FD_GETLogin.php
+++ PHP/FD_GETLogin.php
@@ -1,10 +1,9 @@
 <?php
-
 /**
  * Created by PhpStorm.
  * User: Simone
- * Date: 09/03/2016
- * Time: 19:20
+ * Date: 06/04/2016
+ * Time: 00:10
  */
 
 //Imposto qualsiasi orgine da cui arriva la richiesta come abilitata e la metto in cache per un giorno
@@ -114,8 +113,14 @@ if (strlen($keyRequest) > 0) {
         return;
     }
 
+    //Prendo IP client
+    $url = new FD_Url();
+
+    //Salvo nel log delle richieste GET
+    $sql->executeSQL("call spFD_GetRequest('" . $username . "','" . $password . "','".$url->IP_ADDRESS."');");
+
     //Eseguo la query di login
-    $result = $sql->exportJSON("call spFD_login('" . $username . "','" . md5($password) . "');");
+    $result = $sql->exportJSON("call spFD_login('" . $username . "','" . $password . "');");
 
     if (strlen($sql->lastError) > 0) {
         echo '{"error" : "' . $sql->lastError . '"}';
@@ -131,57 +136,10 @@ if (strlen($keyRequest) > 0) {
         return;
     }
 
-    //Alloco variabile di sessione
-    $random = new FD_Random();
-    $crypt = new FD_Crypt();
-    $array = json_decode($result, true);
-    $date = new DateTime();
-    $token = $crypt->simple_crypt($random->Generate(10).",".$keyRequest.",".$date->format('Y-m-d H:i:s'));
-
-    //Inizializzo la sessione
-    /*
-    session_start();
-    $_SESSION['user_id'] = $array["id"];
-    $_SESSION['username'] = $username;
-    $_SESSION['email'] = $array["email"];
-    $_SESSION['admin'] = $array["admin"];
-    $_SESSION['ID'] = $random->Generate(50);
-    */
-
-    /*
-    //Gestisco l'ID di sessione con lo standard JWT
-    $token = (new Builder())->setIssuer('http://example.com')// Configures the issuer (iss claim)
-    ->setAudience('http://example.org')// Configures the audience (aud claim)
-    ->setId('4f1g23a12aa', true)// Configures the id (jti claim), replicating as a header item
-    ->setIssuedAt(time())// Configures the time that the token was issue (iat claim)
-    ->setNotBefore(time() + 60)// Configures the time that the token can be used (nbf claim)
-    ->setExpiration(time() + 3600)// Configures the expiration time of the token (exp claim)
-    ->set('uid', 1)// Configures a new claim, called "uid"
-    ->getToken(); // Retrieves the generated token
-    $token->getHeaders(); // Retrieves the token headers
-    $token->getClaims(); // Retrieves the token claims
-    echo $token; // The string representation of the object is a JWT string (pretty easy, right?)
-    */
-    /*
-     * unset($_SESSION['username']);
-          unset($_SESSION['email']);
-          unset($_SESSION['name']);
-          unset($_SESSION['userid']);
-          session_destroy();
-          session_regenerate_id();
-     *
-     */
-
-    //Prendo IP client
-    $url = new FD_Url();
-
-    //Salvo nel log delle sessioni
-    $sql->executeSQL("call spFD_session_log(" . $array["id"] . ",'" . $token . "','".$url->IP_ADDRESS."');");
-
     //Chiudo connessione
     $sql->closeConnection();
 
-    echo '{"user":' . $result .',"token": {"token" : "'.$token.'"}}';
+    echo '{"user":' . $result .'}';
 } else {
     echo '{"error" : "Invalid request !""}';
 }
